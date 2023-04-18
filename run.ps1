dec322d96a649820f0e4daf9afee3e3006c96a12
Noble Osinachi <nobleosinachi98@gmail.com>
2023-04-18 15:28:50 +0100

    Last updated on Thursday April 18 2023 at 03:30: PM
    Co-authored-by: David Oluwasegun <oluwasegundavid7@gmail.com>
    Co-authored-by: David Popoola <davieedpopoola@gmail.com>
    Co-authored-by: Ibukun <ibukunoladipupo10@gmail.com>
    Co-authored-by: Isaac Bassey <chartobyson2025@gmail.com>
    Co-authored-by: Jato Joseph <jaotjoseph4@gmail.com>
    Co-authored-by: Joan <sylvesterjoannaunyii@gmail.com>
    Co-authored-by: Joshua-Chris <joshuadgreat04@gmail.com>
    Co-authored-by: Noble Osinachi <150407018@live.unilag.edu.ng>
    Co-authored-by: PraiseGeek <praisegeek@gmail.com>
    Co-authored-by: Precious Adedibu <adedibuprecious@gmail.com>
    Co-authored-by: Sanowar Sunny <sanowarhossain3843@gmail.com>
    Co-authored-by: iAmGolden <chuksgold29@gmail.com>
    Co-authored-by: josiahking <josiahaccounts@gmail.com>
    Co-authored-by: masaun <masaun@protonmail.com>
    Co-authored-by: femotizo <epebinuoluwafemi@gmail.com>
    Signed-off-by: Noble Osinachi <nobleosinachi98@gmail.com>

---
+++ run.ps1
@@ -0,0 +1,36 @@
+
+cd "C:\Users\Noble\SOURCES\REPOS\unyii"
+
+New-Item -ItemType File -Path "logs\1681416551.log" -Force
+
+git pull origin
+
+git add .
+
+git commit -s -m "Last updated on Thursday April 18 2023 at 03:30: PM
+
+Co-authored-by: David Oluwasegun <oluwasegundavid7@gmail.com>
+Co-authored-by: David Popoola <davieedpopoola@gmail.com>
+Co-authored-by: Ibukun <ibukunoladipupo10@gmail.com>
+Co-authored-by: Isaac Bassey <chartobyson2025@gmail.com>
+Co-authored-by: Jato Joseph <jaotjoseph4@gmail.com>
+Co-authored-by: Joan <sylvesterjoannaunyii@gmail.com>
+Co-authored-by: Joshua-Chris <joshuadgreat04@gmail.com>
+Co-authored-by: Noble Osinachi <150407018@live.unilag.edu.ng>
+Co-authored-by: PraiseGeek <praisegeek@gmail.com>
+Co-authored-by: Precious Adedibu <adedibuprecious@gmail.com>
+Co-authored-by: Sanowar Sunny <sanowarhossain3843@gmail.com>
+Co-authored-by: iAmGolden <chuksgold29@gmail.com>
+Co-authored-by: josiahking <josiahaccounts@gmail.com>
+Co-authored-by: masaun <masaun@protonmail.com>
+Co-authored-by: femotizo <epebinuoluwafemi@gmail.com>
+Signed-off-by: Noble Osinachi <nobleosinachi98@gmail.com>"
+
+git push origin
+
+
+
+
+
+
+
